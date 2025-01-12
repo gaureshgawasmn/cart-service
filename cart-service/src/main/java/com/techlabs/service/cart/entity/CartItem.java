dfb49f9db21b1d5154067a3edf78029f2ee3cbab
gaureshgawasmn <gaureshgawasmn@gmail.com>
2025-01-12 11:29:03 +0530

    initial commit

---
+++ cart-service/src/main/java/com/techlabs/service/cart/entity/CartItem.java
@@ -0,0 +1,34 @@
+package com.techlabs.service.cart.entity;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "cart_items")
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_id")
+    private Long productId;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column(name = "price")
+    private double price;
+
+    @Column(name = "quantity")
+    private int quantity;
+}
---
+++ cart-service/src/main/java/com/techlabs/service/cart/entity/UserCart.java
@@ -0,0 +1,39 @@
+package com.techlabs.service.cart.entity;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "user_carts")
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserCart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "cart_id")
+    private List<CartItem> items = new ArrayList<>();
+
+    @Column(name = "total_price")
+    private double totalPrice;
+}
